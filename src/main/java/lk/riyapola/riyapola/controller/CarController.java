c620e87c28fa93c98272b3d05f25375bf07a0c04
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-03-22 12:46:55 +0530

    change @RequestMapping url in carController

--- src/main/java/lk/riyapola/riyapola/controller/CarController.java
+++ src/main/java/lk/riyapola/riyapola/controller/CarController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @CrossOrigin
 @RestController
-@RequestMapping("admin/car")
+@RequestMapping("riyapola/car")
 
 public class CarController {
     private final CarService carService;
