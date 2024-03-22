514c830e757afbeb95a5383b64c4dabe398cb43b
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-03-22 11:55:07 +0530

    change url in car access

--- src/main/java/lk/riyapola/riyapola/controller/CarController.java
+++ src/main/java/lk/riyapola/riyapola/controller/CarController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @CrossOrigin
 @RestController
-@RequestMapping("/car")
+@RequestMapping("admin/car")
 
 public class CarController {
     private final CarService carService;
